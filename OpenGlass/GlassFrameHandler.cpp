7fa27ff21ec650e8dbd2cb2626310ef4cb804c22
ALTaleX <105535231+ALTaleX531@users.noreply.github.com>
2026-06-04 08:13:41 +0800

    Hook CLegacyNonClientBackground lifecycle to drive effect and reflection visuals

--- OpenGlass/GlassFrameHandler.cpp
+++ OpenGlass/GlassFrameHandler.cpp
@@ -311,251 +311,267 @@ HRESULT GlassFrameHandler::MyCGlassColorizationParameters_AdjustWindowColorizati
 	return S_OK;
 }
 
-HRESULT GlassFrameHandler::MyCTopLevelWindow_UpdateNCAreaBackground(uDWM::CTopLevelWindow* This)
+
+HRESULT GlassFrameHandler::MyCChannel_CombinedGeometryUpdate(
+	dwmcore::CChannel* This,
+	UINT handleId,
+	D2D1_COMBINE_MODE mode,
+	UINT geometry1HandleId,
+	UINT geometry2HandleId
+)
 {
-	const auto active = This->TreatAsActiveWindow();
-	const auto maximized = This->TreatAsMaximized();
-
-	HRESULT hr{ S_OK };
-	GlassKernel::g_redirectFirstCreateRectRgnCall = true;
-	g_combinedRgn.reset(CreateRectRgn(0, 0, 0, 0));
-	const auto combinedRgnScope = wil::scope_exit([] { GlassKernel::g_redirectFirstCreateRectRgnCall = std::nullopt; g_combinedRgn.reset(); });
-	hr = g_CTopLevelWindow_UpdateNCAreaBackground_Org(This);
-
-	auto effectBrush = GlassEffectBrush::GetOrCreate(This);
-
-	if (
-		const auto legacyVisual = This->GetLegacyVisual();
-		legacyVisual &&
-		SUCCEEDED(legacyVisual->_ValidateVisual()) &&
-		!legacyVisual->IsEmpty()
-	)
+	if (GlassKernel::g_window)
 	{
-		const auto reflectionBrush = GlassReflectionBrush::GetOrCreate(
-			This,
-			0,
-			true
+		std::bitset<2> windowStatus{};
+		windowStatus.set(0, GlassKernel::g_window->TreatAsActiveWindow());
+		windowStatus.set(1, GlassKernel::g_window->TreatAsMaximized());
+
+		mode = static_cast<D2D1_COMBINE_MODE>(windowStatus.to_ulong());
+	}
+
+	return g_CChannel_CombinedGeometryUpdate_Org(This, handleId, mode, geometry1HandleId, geometry2HandleId);
+}
+void GlassFrameHandler::MyCLegacyNonClientBackground_ClearAll(uDWM::CLegacyNonClientBackground* This)
+{
+	RemoveReflectionVisual(uDWM::TryGetWindowFromVisual(This), 0);
+	RemoveEffectVisual(This);
+	return g_CLegacyNonClientBackground_ClearAll_Org(This);
+}
+bool GlassFrameHandler::MyCLegacyNonClientBackground_HasSomethingToRender(uDWM::CLegacyNonClientBackground* This)
+{
+	return g_effectVisualMap.find(This) != g_effectVisualMap.end();
+}
+HRESULT GlassFrameHandler::MyCLegacyNonClientBackground_SetCaptionRect(uDWM::CLegacyNonClientBackground* This, LPCRECT rc)
+{
+	const auto window = uDWM::TryGetWindowFromVisual(This);
+	if (!IsRectEmpty(&g_outerBorderRect))
+	{
+		const auto effectVisual = GetOrCreateEffectVisual(This, true);
+		const auto reflectionVisual = GetOrCreateReflectionVisual(window, 0, effectVisual.get(), true);
+
+		wil::unique_hrgn windowRgn{ nullptr };
+		wil::unique_hrgn clientRgn{ nullptr };
+
+		const auto diameter = Shared::g_roundRectRadius * 2;
+		// HACK: there is a fxxking gdi bug introduced in windows 11 25h2 that causes CreateRoundRectRgn to shrink the region by 1 pixel in both dimensions,
+		// which is exactly the case for our border region when rounded corners are enabled,
+		// causing our effect visual to be clipped by 1 pixel on the right and bottom sides.
+		// As a workaround, we can simply add 1 pixel to the width and height when creating the rounded region,
+		// which does not cause any visual issue but can avoid the GDI bug. 
+		windowRgn.reset(
+			diameter ?
+			CreateRoundRectRgn(
+				g_outerBorderRect.left,
+				g_outerBorderRect.top,
+				g_outerBorderRect.right + 1,
+				g_outerBorderRect.bottom + 1,
+				diameter,
+				diameter
+			) :
+			CreateRectRgn(
+				g_outerBorderRect.left,
+				g_outerBorderRect.top,
+				g_outerBorderRect.right,
+				g_outerBorderRect.bottom
+			)
 		);
-		if (!effectBrush)
-		{
-			effectBrush = GlassEffectBrush::GetOrCreate(This, true);
+		RETURN_LAST_ERROR_IF_NULL(windowRgn);
 
-			if (Shared::g_overrideAccent)
-			{
-				if (
-					const auto accentVisual = This->GetAccent();
-					accentVisual
-				)
-				{
-					accentVisual->SetDirtyFlags(0x1000);
-				}
-			}
-		}
+		bool isSheetOfGlass = g_innerBorderRect.left == 0 && g_innerBorderRect.top == 0 && g_innerBorderRect.right == 0 && g_innerBorderRect.bottom == 0;
+		clientRgn.reset(
+			isSheetOfGlass ?
+			CreateRectRgn(0, 0, 0, 0) :
+			CreateRectRgn(
+				rc->left,
+				rc->bottom,
+				rc->right,
+				g_innerBorderRect.bottom
+			)
+		);
+		RETURN_LAST_ERROR_IF_NULL(clientRgn);
 
-		winrt::com_ptr<uDWM::CRgnGeometryProxy> captionGeometry{ nullptr };
-		winrt::com_ptr<uDWM::CBaseLegacyMilBrushProxy> brush{ nullptr };
-		{
-			const auto instruction = reinterpret_cast<uDWM::CDrawGeometryInstruction*>(legacyVisual->GetInstructions().views().front());
-			brush.copy_from(instruction->GetBrush());
-			captionGeometry.copy_from(reinterpret_cast<uDWM::CRgnGeometryProxy*>(instruction->GetGeometry()));
-			RETURN_IF_FAILED(
-				uDWM::ResourceHelper::CreateGeometryFromHRGN(
-					g_combinedRgn.get(),
-					reinterpret_cast<uDWM::CRgnGeometryProxy**>(&captionGeometry)
-				)
-			);
-		}
+		RETURN_IF_WIN32_BOOL_FALSE(CombineRgn(windowRgn.get(), windowRgn.get(), clientRgn.get(), RGN_DIFF));
 
-		if (brush.get() != effectBrush.get())
-		{
-			RETURN_IF_FAILED(legacyVisual->ClearInstructions());
-			winrt::com_ptr<uDWM::CDrawGeometryInstruction> instruction{};
-			RETURN_IF_FAILED(
-				uDWM::CDrawGeometryInstruction::Create(
-					effectBrush.get(),
-					captionGeometry.get(),
-					instruction.put()
-				)
-			);
-			RETURN_IF_FAILED(legacyVisual->AddInstruction(instruction.get()));
-			RETURN_IF_FAILED(
-				uDWM::CDrawGeometryInstruction::Create(
-					reflectionBrush.get(),
-					captionGeometry.get(),
-					instruction.put()
-				)
-			);
-			RETURN_IF_FAILED(legacyVisual->AddInstruction(instruction.get()));
-		}
+		RECT rgnBox{};
+		RETURN_LAST_ERROR_IF(GetRgnBox(windowRgn.get(), &rgnBox) == NULLREGION);
+		POINT offset{ 0, 0 };
+		SIZE size{ rgnBox.right, rgnBox.bottom };
 
-		if (!This->IsOffscreen())
+		// NOTE: you MUST use SetRect otherwise the entire visual will be invisible and you will have higher posibility to encounter mysterious compositor bug.
+		D2D1_RECT_F rect
 		{
-			RETURN_IF_FAILED(
-				reflectionBrush->Update(
-					(Shared::g_reflectionPolicy & Shared::ReflectionPolicy::NonClient) ?
-					GlassKernel::GetAdjustedReflectionIntensity(active, maximized) :
-					0.f,
-					GlassReflectionBrush::CalculateTargetViewport(
-						legacyVisual->GetLocalToParentVisualOffset(This->GetTransformParent()),
-						Shared::g_reflectionParallaxIntensity,
-						This->IsRTLMirrored(),
-						legacyVisual->GetWidth(),
-						legacyVisual->GetScale()
-					),
-					D2D1::RectF(),
-					nullptr,
-					DWM::MilBrushMappingMode::Absolute,
-					DWM::MilBrushMappingMode::Absolute,
-					nullptr,
-					nullptr,
-					DWM::MilStretch::None,
-					DWM::MilTileMode::Extend,
-					DWM::MilHorizontalAlignment::Left,
-					DWM::MilVerticalAlignment::Top,
-					nullptr
-				)
-			);
-		}
+			static_cast<float>(0.f),
+			static_cast<float>(0.f),
+			static_cast<float>(rgnBox.right),
+			static_cast<float>(rgnBox.bottom)
+		};
+		effectVisual->SetRect(rect);
+		//effectVisual->SetOffset(&offset);
+
+		winrt::com_ptr<uDWM::CRgnGeometryProxy> regionGeometry{};
+		RETURN_IF_FAILED(uDWM::ResourceHelper::CreateGeometryFromHRGN(windowRgn.get(), regionGeometry.put()));
+
+		winrt::com_ptr<uDWM::CCombinedGeometryProxy> combinedGeometry{};
+		RETURN_IF_FAILED(uDWM::ResourceHelper::CreateCombinedGeometry(regionGeometry.get(), nullptr, D2D1_COMBINE_MODE_INTERSECT, combinedGeometry.put()));
+		RETURN_IF_FAILED(effectVisual->UpdateClip(combinedGeometry.get()));
+
+		reflectionVisual->SetSize(&size);
+
+		RETURN_IF_FAILED(reflectionVisual->UpdateSurface(GlassKernel::GetOrCreateReflectionSurface()));
+
+		RETURN_IF_FAILED(
+			reflectionVisual->UpdateViewport(
+				reflectionVisual->GetLocalToParentVisualOffset(window->GetTransformParent()),
+				Shared::g_reflectionParallaxIntensity,
+				window->IsRTLMirrored(),
+				size.cx,
+				window->GetScale()
+			)
+		);
+
+		RETURN_IF_FAILED(
+			reflectionVisual->UpdateOpacity(
+				Shared::g_reflectionPolicy & Shared::ReflectionPolicy::NonClient ?
+				GlassKernel::GetAdjustedReflectionIntensity(
+					window->TreatAsActiveWindow(),
+					window->TreatAsMaximized()
+				) :
+				0.f
+			)
+		);
 	}
-
-	// if fTransitionOnMaximized is specified,
-	// we should also update the brush even the nonclient area is empty
-	if (effectBrush)
+	else
 	{
-		auto color = This->GetCaptionColorizationParameters()->getArgbcolor();
-		color.a = GlassKernel::AlphaChannelReinterpreter(active, maximized).ToFloat();
-		RETURN_IF_FAILED(effectBrush->Update(1.0, color));
-	}
-	else if (Shared::g_overrideAccent)
-	{
-		if (
-			const auto accentVisual = This->GetAccent();
-			accentVisual
-		)
-		{
-			accentVisual->SetDirtyFlags(0x1000);
-		}
+		RemoveReflectionVisual(window, 0);
+		RemoveEffectVisual(This);
 	}
 
-	return hr;
+	RECT emptyRect{};
+	return g_CLegacyNonClientBackground_SetCaptionRect_Org(This, &emptyRect);
+}
+HRESULT GlassFrameHandler::MyCLegacyNonClientBackground_SetBorderRects(uDWM::CLegacyNonClientBackground* This, LPCRECT rc1, LPCRECT rc2)
+{
+	// patch microsoft's terrible code
+	// IsSheetOfGlass == true && IsHighContrastMode == true
+	// 
+	/*bool isSheetOfGlass = rc1->left == 0 && rc1->top == 0 && rc1->right == 0 && rc1->bottom == 0;
+	if (isSheetOfGlass)
+	{
+		const auto center = (rc2->left + rc2->right) / 2;
+		const auto middle = (rc2->top + rc2->bottom) / 2;
+		const_cast<LPRECT>(rc1)->left = center;
+		const_cast<LPRECT>(rc1)->right = center;
+		const_cast<LPRECT>(rc1)->top = middle;
+		const_cast<LPRECT>(rc1)->bottom = middle;
+	}*/
+
+	g_innerBorderRect = *rc1;
+	g_outerBorderRect = *rc2;
+
+	RECT emptyRect{};
+	return g_CLegacyNonClientBackground_SetBorderRects_Org(This, &emptyRect, &emptyRect);
+}
+HRESULT GlassFrameHandler::MyCLegacyNonClientBackground_SetCaptionColor(uDWM::CLegacyNonClientBackground* This, const D2D1_COLOR_F& color)
+{
+	const auto effectVisual = GetOrCreateEffectVisual(This, false);
+	if (effectVisual)
+	{
+		RETURN_IF_FAILED(effectVisual->UpdateColor(color));
+	}
+
+	return g_CLegacyNonClientBackground_SetCaptionColor_Org(This, color);
+}
+void GlassFrameHandler::MyCLegacyNonClientBackground_Destructor(uDWM::CLegacyNonClientBackground* This)
+{
+	const auto window = uDWM::TryGetWindowFromVisual(This);
+	RemoveReflectionVisual(window, 0);
+	RemoveEffectVisual(This);
+	return g_CLegacyNonClientBackground_Destructor_Org(This);
 }
 
+bool GlassFrameHandler::MyCRectangleVisual_SetRect(uDWM::CRectangleVisual* This, const D2D1_RECT_F& rc)
+{
+	const auto window = uDWM::TryGetWindowFromVisual(This);
+	if (
+		!window ||
+		window->GetClientBlurVisual() != This
+	)
+	{
+		return g_CRectangleVisual_SetRect_Org(This, rc);
+	}
+
+	const auto reflectionVisual = GetOrCreateReflectionVisual(window, 1, This, true);
+
+	SIZE size{ static_cast<LONG>(rc.right), static_cast<LONG>(rc.bottom) };
+	reflectionVisual->SetSize(&size);
+
+	RETURN_IF_FAILED(reflectionVisual->UpdateSurface(GlassKernel::GetOrCreateReflectionSurface()));
+	RETURN_IF_FAILED(
+		reflectionVisual->UpdateViewport(
+			reflectionVisual->GetLocalToParentVisualOffset(window->GetTransformParent()),
+			Shared::g_reflectionParallaxIntensity,
+			window->IsRTLMirrored(),
+			size.cx,
+			window->GetScale()
+		)
+	);
+
+	RETURN_IF_FAILED(
+		reflectionVisual->UpdateOpacity(
+			Shared::g_reflectionPolicy & Shared::ReflectionPolicy::NonClient ?
+			GlassKernel::GetAdjustedReflectionIntensity(
+				window->TreatAsActiveWindow(),
+				window->TreatAsMaximized()
+			) :
+			0.f
+		)
+	);
+
+	// HACK: SetRect is not SetViewbox, 
+	// microsoft's code mistakenly applies the offset to the rect,
+	// which causes the blur to be misaligned when DwmExtendFrameIntoClientArea is used.
+	auto adjustedRect = rc;
+	adjustedRect.left = adjustedRect.top = 0;
+	return g_CRectangleVisual_SetRect_Org(This, adjustedRect);
+}
 HRESULT GlassFrameHandler::MyCTopLevelWindow_UpdateClientBlur(uDWM::CTopLevelWindow* This)
 {
 	const auto hr = g_CTopLevelWindow_UpdateClientBlur_Org(This);
 
-	const auto active = This->TreatAsActiveWindow();
-	const auto maximized = This->TreatAsMaximized();
-
-	auto effectBrush = GlassEffectBrush::GetOrCreate(This);
-
-	if (
-		const auto clientBlurVisual = This->GetClientBlurVisual();
-		clientBlurVisual &&
-		!clientBlurVisual->IsEmpty()
-	)
+	if (const auto clientBlurVisual = This->GetClientBlurVisual(); !clientBlurVisual)
 	{
-		const auto reflectionBrush = GlassReflectionBrush::GetOrCreate(
-			This,
-			1,
-			true
-		);
-		if (!effectBrush)
-		{
-			effectBrush = GlassEffectBrush::GetOrCreate(This, true);
-
-			if (Shared::g_overrideAccent)
-			{
-				if (
-					const auto accentVisual = This->GetAccent();
-					accentVisual
-				)
-				{
-					accentVisual->SetDirtyFlags(0x1000);
-				}
-			}
-		}
-
-		winrt::com_ptr<uDWM::CRgnGeometryProxy> clientBlurGeometry{ nullptr };
-		winrt::com_ptr<uDWM::CBaseLegacyMilBrushProxy> brush{ nullptr };
-		{
-			const auto instruction = reinterpret_cast<uDWM::CDrawGeometryInstruction*>(clientBlurVisual->GetInstructions().views().front());
-			brush.copy_from(instruction->GetBrush());
-			clientBlurGeometry.copy_from(reinterpret_cast<uDWM::CRgnGeometryProxy*>(instruction->GetGeometry()));
-		}
-
-		if (brush.get() != effectBrush.get())
-		{
-			RETURN_IF_FAILED(clientBlurVisual->ClearInstructions());
-			winrt::com_ptr<uDWM::CDrawGeometryInstruction> instruction{};
-			RETURN_IF_FAILED(
-				uDWM::CDrawGeometryInstruction::Create(
-					effectBrush.get(),
-					clientBlurGeometry.get(),
-					instruction.put()
-				)
-			);
-			RETURN_IF_FAILED(clientBlurVisual->AddInstruction(instruction.get()));
-			RETURN_IF_FAILED(
-				uDWM::CDrawGeometryInstruction::Create(
-					reflectionBrush.get(),
-					clientBlurGeometry.get(),
-					instruction.put()
-				)
-			);
-			RETURN_IF_FAILED(clientBlurVisual->AddInstruction(instruction.get()));
-		}
-
-		if (!This->IsOffscreen())
-		{
-			RETURN_IF_FAILED(
-				reflectionBrush->Update(
-					(Shared::g_reflectionPolicy & Shared::ReflectionPolicy::NonClient) ?
-					GlassKernel::GetAdjustedReflectionIntensity(active, maximized) :
-					0.f,
-					GlassReflectionBrush::CalculateTargetViewport(
-						clientBlurVisual->GetLocalToParentVisualOffset(This->GetTransformParent()),
-						Shared::g_reflectionParallaxIntensity,
-						This->IsRTLMirrored(),
-						clientBlurVisual->GetWidth(),
-						clientBlurVisual->GetScale()
-					),
-					D2D1::RectF(),
-					nullptr,
-					DWM::MilBrushMappingMode::Absolute,
-					DWM::MilBrushMappingMode::Absolute,
-					nullptr,
-					nullptr,
-					DWM::MilStretch::None,
-					DWM::MilTileMode::Extend,
-					DWM::MilHorizontalAlignment::Left,
-					DWM::MilVerticalAlignment::Top,
-					nullptr
-				)
-			);
-		}
+		RemoveReflectionVisual(This, 1);
 	}
-
-	// if fTransitionOnMaximized is specified,
-	// we should also update the brush even the nonclient area is empty
-	if (effectBrush)
-	{
-		auto color = This->GetCaptionColorizationParameters()->getArgbcolor();
-		color.a = GlassKernel::AlphaChannelReinterpreter(active, maximized).ToFloat();
-		RETURN_IF_FAILED(effectBrush->Update(1.0, color));
-	}
-	else if (Shared::g_overrideAccent)
-	{
-		if (
-			const auto accentVisual = This->GetAccent();
-			accentVisual
-		)
-		{
-			accentVisual->SetDirtyFlags(0x1000);
-		}
-	}
-
 	return hr;
+}
+
+HRESULT GlassFrameHandler::MyCTopLevelWindow_UpdateNCAreaBackground(uDWM::CTopLevelWindow* This)
+{
+	// HACK: UpdateClientBlur always checks current dirty flags to decide whether to update blur or not,
+	// in some cases (e.g. when window is maximized/activated) the window states can be changed without UpdateClientBlur being called,
+	// which causes the glass to not update and thus look wrong.
+	// Forcing an UpdateClientBlur call here seems to fix the issue.
+	This->OnBlurBehindUpdated();
+	return g_CTopLevelWindow_UpdateNCAreaBackground_Org(This);
+}
+
+bool GlassFrameHandler::MyCTopLevelWindow_EdgeBorderMustBeOpaque(uDWM::CTopLevelWindow* This)
+{
+	// When the window is inactive and EdgeBorderMustBeOpaque is false,
+	// the border rect computed in CTopLevelWindow::UpdateNCAreaGeometry shrinks to zero (border visual removed, only caption remains).
+	// This shrinks the visual's SetRect to caption size, which causes the occlusion pass to pass because
+	// COcclusionContext::PreSubgraph contains a size threshold optimization at 0x180069838:
+	//   comiss xmm10, 74752.0   ; visual area vs threshold
+	//   jb    skip_check         ; area < 74752 -> bypass the stricter occlusion checks
+	// When SetRect is set to the full window rect, the visual area exceeds 74752 px^2,
+	// triggering additional validation that disables occlusion for the subtree.
+
+	if (Shared::g_dontDeflateInactiveFrameGeometry)
+	{
+		return true;
+	}
+
+	return g_CTopLevelWindow_EdgeBorderMustBeOpaque_Org(This);
 }
 
 HRESULT GlassFrameHandler::MyCTopLevelWindow_ValidateVisual(uDWM::CTopLevelWindow* This)
@@ -567,19 +583,15 @@ HRESULT GlassFrameHandler::MyCTopLevelWindow_ValidateVisual(uDWM::CTopLevelWindo
 	}
 
 	GlassKernel::g_window = This;
-	const auto updateReflectionBeforeLeave = wil::scope_exit([This]
-	{
-		GlassKernel::g_window = nullptr;
-		LOG_IF_FAILED(UpdateReflectionViewport(This));
-	});
-	
-	return g_CTopLevelWindow_ValidateVisual_Org(This);
+	const auto hr = g_CTopLevelWindow_ValidateVisual_Org(This);
+	LOG_IF_FAILED(UpdateReflectionViewport(This));
+	GlassKernel::g_window = nullptr;
+	return hr;
 }
 
 void GlassFrameHandler::MyCTopLevelWindow_Destructor(uDWM::CTopLevelWindow* This)
 {
-	GlassReflectionBrush::Remove(This);
-	GlassEffectBrush::Remove(This);
+	RemoveReflectionVisual(This, -1);
 	return g_CTopLevelWindow_Destructor_Org(This);
 }
 
